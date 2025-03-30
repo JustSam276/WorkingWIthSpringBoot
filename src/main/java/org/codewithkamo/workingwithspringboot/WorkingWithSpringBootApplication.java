338985d2d7652c8130fbd4367b2d52948aade63d
JustSam <samkamoheloratlou.final@gmail.com>
2025-03-30 11:14:43 +0200

    Basic set up of spring boot, with Services and a Controller

---
+++ src/main/java/org/codewithkamo/workingwithspringboot/WorkingWithSpringBootApplication.java
@@ -0,0 +1,14 @@
+package org.codewithkamo.workingwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        SpringApplication.run(WorkingWithSpringBootApplication.class, args);
+    }
+
+}
